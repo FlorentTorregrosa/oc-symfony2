6d37193657e29574cb9ae611231687221545407a
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-10-08 20:12:13 +0200

    2.3: Manipulate the session.

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,7 +3,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
@@ -15,9 +15,19 @@ class AdvertController extends Controller
         return new Response($content);
     }
 
-    public function viewAction($id)
+    public function viewAction($id, Request $request)
     {
-        return new JsonResponse(array('id' => $id));
+        // Récupération de la session
+        $session = $request->getSession();
+
+        // On récupère le contenu de la variable user_id
+        $userId = $session->get('user_id');
+
+        // On définit une nouvelle valeur pour cette variable user_id
+        $session->set('user_id', 91);
+
+        // On n'oublie pas de renvoyer une réponse
+        return new Response("Je suis une page de test, je n'ai rien à dire");
     }
 
 }
