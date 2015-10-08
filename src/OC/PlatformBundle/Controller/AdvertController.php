d92302108fa988ff9a4d02a83774ebc8fd27d852
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-10-08 19:49:57 +0200

    2.3: Use template for view action.

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -17,17 +17,10 @@ class AdvertController extends Controller
 
     public function viewAction($id)
     {
-        // On crée la réponse sans lui donner de contenu pour le moment
-        $response = new Response;
-
-        // On définit le contenu
-        $response->setContent("Ceci est une page d'erreur 404");
-
-        // On définit le code HTTP à « Not Found » (erreur 404)
-        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-
-        // On retourne la réponse
-        return $response;
+        return $this->render(
+          'OCPlatformBundle:Advert:view.html.twig',
+          array('id' => $id)
+        );
     }
 
 }
