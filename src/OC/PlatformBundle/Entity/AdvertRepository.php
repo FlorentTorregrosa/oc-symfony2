3da79f9194b9a8f099d7383ea4ecd55f2998a099
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-10-09 22:31:25 +0200

    3.4: Add functions to advert repository.

--- src/OC/PlatformBundle/Entity/AdvertRepository.php
+++ src/OC/PlatformBundle/Entity/AdvertRepository.php
@@ -2,6 +2,8 @@
 
 namespace OC\PlatformBundle\Entity;
 
+use Doctrine\ORM\QueryBuilder;
+
 /**
  * AdvertRepository
  *
@@ -27,6 +29,54 @@ class AdvertRepository extends \Doctrine\ORM\EntityRepository
           ->where('a.id = :id')
           ->setParameter('id', $id)
         ;
+
+        return $qb
+          ->getQuery()
+          ->getResult()
+          ;
+    }
+
+    public function findByAuthorAndDate($author, $year)
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        $qb->where('a.author = :author')
+          ->setParameter('author', $author)
+          ->andWhere('a.date < :year')
+          ->setParameter('year', $year)
+          ->orderBy('a.date', 'DESC')
+        ;
+
+        return $qb
+          ->getQuery()
+          ->getResult()
+          ;
+    }
+
+    public function whereCurrentYear(QueryBuilder $qb)
+    {
+        $qb
+          ->andWhere('a.date BETWEEN :start AND :end')
+          ->setParameter('start', new \Datetime(date('Y').'-01-01'))  // Date entre le 1er janvier de cette année
+          ->setParameter('end',   new \Datetime(date('Y').'-12-31'))  // Et le 31 décembre de cette année
+        ;
+    }
+
+    public function myFind()
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        // On peut ajouter ce qu'on veut avant
+        $qb
+          ->where('a.author = :author')
+          ->setParameter('author', 'Marine')
+        ;
+
+        // On applique notre condition sur le QueryBuilder
+        $this->whereCurrentYear($qb);
+
+        // On peut ajouter ce qu'on veut après
+        $qb->orderBy('a.date', 'DESC');
 
         return $qb
           ->getQuery()
