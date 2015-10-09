fb92e446d17127e8a47bbe1d571bf72222c8196a
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-10-09 22:47:55 +0200

    3.4: Add functions in repositories.

--- src/OC/PlatformBundle/Entity/ApplicationRepository.php
+++ src/OC/PlatformBundle/Entity/ApplicationRepository.php
@@ -10,4 +10,24 @@ namespace OC\PlatformBundle\Entity;
  */
 class ApplicationRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getApplicationsWithAdvert($limit)
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        // On fait une jointure avec l'entité Advert avec pour alias « adv »
+        $qb
+          ->join('a.advert', 'adv')
+          ->addSelect('adv')
+        ;
+
+        // Puis on ne retourne que $limit résultats
+        $qb->setMaxResults($limit);
+
+        // Enfin, on retourne le résultat
+        return $qb
+          ->getQuery()
+          ->getResult()
+          ;
+    }
+
 }
