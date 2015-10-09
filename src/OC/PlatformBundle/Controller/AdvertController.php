4a17ad4aa13645d690c3e22def326203fa1938fe
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-10-09 13:48:28 +0200

    3.2: Use entitymanager in add action.

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -2,6 +2,7 @@
 
 namespace OC\PlatformBundle\Controller;
 
+use OC\PlatformBundle\Entity\Advert;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -64,28 +65,29 @@ class AdvertController extends Controller
 
     public function addAction(Request $request)
     {
-        // On récupère le service
-        $antispam = $this->container->get('oc_platform.antispam');
+        // Création de l'entité
+        $advert = new Advert();
+        $advert->setTitle('Recherche développeur Symfony2.');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
 
-        // Je pars du principe que $text contient le texte d'un message quelconque
-        $text = '...';
-        if ($antispam->isSpam($text)) {
-            throw new \Exception('Votre message a été détecté comme spam !');
-        }
+        // On peut ne pas définir ni la date ni la publication,
+        // car ces attributs sont définis automatiquement dans le constructeur
 
-        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
 
-        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        // Étape 1 : On « persiste » l'entité
+        $em->persist($advert);
+
+        // Étape 2 : On « flush » tout ce qui a été persisté avant
+        $em->flush();
+
+        // Reste de la méthode qu'on avait déjà écrit
         if ($request->isMethod('POST')) {
-            // Ici, on s'occupera de la création et de la gestion du formulaire
-
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-
-            // Puis on redirige vers la page de visualisation de cettte annonce
-            return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+            return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
         }
-
-        // Si on n'est pas en POST, alors on affiche le formulaire
         return $this->render('OCPlatformBundle:Advert:add.html.twig');
     }
 
