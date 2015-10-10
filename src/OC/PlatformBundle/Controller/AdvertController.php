d09949f9183bf38df3d1746c2c3a592735bde4a2
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-10-10 19:05:34 +0200

    3.6: Add pager to index.

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -18,16 +18,30 @@ class AdvertController extends Controller
             throw $this->createNotFoundException("La page ".$page." n'existe pas.");
         }
 
-        // Pour récupérer la liste de toutes les annonces : on utilise findAll()
+        // Ici je fixe le nombre d'annonces par page à 3
+        // Mais bien sûr il faudrait utiliser un paramètre, et y accéder via $this->container->getParameter('nb_per_page')
+        $nbPerPage = 3;
+
+        // On récupère notre objet Paginator
         $listAdverts = $this->getDoctrine()
           ->getManager()
           ->getRepository('OCPlatformBundle:Advert')
-          ->getAdverts()
+          ->getAdverts($page, $nbPerPage)
         ;
 
-        // L'appel de la vue ne change pas
+        // On calcule le nombre total de pages grâce au count($listAdverts) qui retourne le nombre total d'annonces
+        $nbPages = ceil(count($listAdverts)/$nbPerPage);
+
+        // Si la page n'existe pas, on retourne une 404
+        if ($page > $nbPages) {
+            throw $this->createNotFoundException("La page ".$page." n'existe pas.");
+        }
+
+        // On donne toutes les informations nécessaires à la vue
         return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
-          'listAdverts' => $listAdverts
+          'listAdverts' => $listAdverts,
+          'nbPages'     => $nbPages,
+          'page'        => $page
         ));
     }
 
--- src/OC/PlatformBundle/Entity/AdvertRepository.php
+++ src/OC/PlatformBundle/Entity/AdvertRepository.php
@@ -3,6 +3,7 @@
 namespace OC\PlatformBundle\Entity;
 
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * AdvertRepository
@@ -136,20 +137,26 @@ class AdvertRepository extends \Doctrine\ORM\EntityRepository
           ;
     }
 
-    public function getAdverts()
+    public function getAdverts($page, $nbPerPage)
     {
         $query = $this->createQueryBuilder('a')
-          // Jointure sur l'attribut image
           ->leftJoin('a.image', 'i')
           ->addSelect('i')
-          // Jointure sur l'attribut categories
           ->leftJoin('a.categories', 'c')
           ->addSelect('c')
           ->orderBy('a.date', 'DESC')
           ->getQuery()
         ;
 
-        return $query->getResult();
+        $query
+          // On définit l'annonce à partir de laquelle commencer la liste
+          ->setFirstResult(($page-1) * $nbPerPage)
+          // Ainsi que le nombre d'annonce à afficher sur une page
+          ->setMaxResults($nbPerPage)
+        ;
+
+        // Enfin, on retourne l'objet Paginator correspondant à la requête construite
+        return new Paginator($query, true);
     }
 
 }
