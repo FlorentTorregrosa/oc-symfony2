44636a4e19d74e3b99c6b63d5305fe43637cbe8c
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-12-27 18:01:10 +0100

    4.1: Convert collection into entity field.

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -21,16 +21,10 @@ class AdvertType extends AbstractType
           ->add('content',   'textarea')
           ->add('published', 'checkbox', array('required' => false))
           ->add('image',      new ImageType())
-          /*
-           * Rappel :
-           ** - 1er argument : nom du champ, ici « categories », car c'est le nom de l'attribut
-           ** - 2e argument : type du champ, ici « collection » qui est une liste de quelque chose
-           ** - 3e argument : tableau d'options du champ
-           */
-          ->add('categories', 'collection', array(
-            'type'         => new CategoryType(),
-            'allow_add'    => true,
-            'allow_delete' => true
+          ->add('categories', 'entity', array(
+            'class'    => 'OCPlatformBundle:Category',
+            'property' => 'name',
+            'multiple' => true
           ))
           ->add('save',      'submit')
         ;
