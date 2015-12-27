d847a7b7f0819d3911c81337fb34d9bc9476cb37
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-12-27 16:37:12 +0100

    4.1: Handle form submit.

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -70,29 +70,40 @@ class AdvertController extends Controller
 
     public function addAction(Request $request)
     {
-        // On crée un objet Advert
         $advert = new Advert();
 
-        // On crée le FormBuilder grâce au service form factory
-        $formBuilder = $this->get('form.factory')->createBuilder('form', $advert);
-
-        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-        $formBuilder
+        // J'ai raccourci cette partie, car c'est plus rapide à écrire !
+        $form = $this->get('form.factory')->createBuilder('form', $advert)
           ->add('date',      'date')
           ->add('title',     'text')
           ->add('content',   'textarea')
           ->add('author',    'text')
-          ->add('published', 'checkbox')
+          ->add('published', 'checkbox', array('required' => false))
           ->add('save',      'submit')
+          ->getForm()
         ;
 
-        // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
+        // On fait le lien Requête <-> Formulaire
+        // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+        $form->handleRequest($request);
 
-        // À partir du formBuilder, on génère le formulaire
-        $form = $formBuilder->getForm();
+        // On vérifie que les valeurs entrées sont correctes
+        // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+        if ($form->isValid()) {
+            // On l'enregistre notre objet $advert dans la base de données, par exemple
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($advert);
+            $em->flush();
 
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            // On redirige vers la page de visualisation de l'annonce nouvellement créée
+            return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+        }
+
+        // À ce stade, le formulaire n'est pas valide car :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
         return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
           'form' => $form->createView(),
         ));
