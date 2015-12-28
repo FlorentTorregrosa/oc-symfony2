2624c049ffdd5860d0ba2d27f88c3c085a6fdd72
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-12-28 23:34:52 +0100

    4.5: Convert event listener into event subscriber.

--- src/OC/PlatformBundle/Bigbrother/CensorshipListener.php
+++ src/OC/PlatformBundle/Bigbrother/CensorshipListener.php
@@ -2,7 +2,9 @@
 
 namespace OC\PlatformBundle\Bigbrother;
 
-class CensorshipListener
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CensorshipListener implements EventSubscriberInterface
 {
     protected $processor;
     protected $listUsers = array();
@@ -11,6 +13,15 @@ class CensorshipListener
     {
         $this->processor = $processor;
         $this->listUsers = $listUsers;
+    }
+
+    // La méthode de l'interface que l'on doit implémenter, à définir en static
+    static public function getSubscribedEvents()
+    {
+        // On retourne un tableau « nom de l'évènement » => « méthode à exécuter »
+        return array(
+          'oc_platform.bigbrother.post_message' => 'processMessage',
+        );
     }
 
     public function processMessage(MessagePostEvent $event)
