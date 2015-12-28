9231eb7bc5cf238a61c7c71e54490ccb02ea9648
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-12-28 19:59:50 +0100

    4.4: Convert Antispam into twig extension.

--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -2,7 +2,7 @@
 
 namespace OC\PlatformBundle\Antispam;
 
-class OCAntispam
+class OCAntispam extends \Twig_Extension
 {
     private $mailer;
     private $locale;
@@ -24,5 +24,19 @@ class OCAntispam
     public function isSpam($text)
     {
         return strlen($text) < $this->minLength;
+    }
+
+    // Twig va exécuter cette méthode pour savoir quelle(s) fonction(s) ajoute notre service
+    public function getFunctions()
+    {
+        return array(
+          'checkIfSpam' => new \Twig_Function_Method($this, 'isSpam')
+        );
+    }
+
+    // La méthode getName() identifie votre extension Twig, elle est obligatoire
+    public function getName()
+    {
+        return 'OCAntispam';
     }
 }
