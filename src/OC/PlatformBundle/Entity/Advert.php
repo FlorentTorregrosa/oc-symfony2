385abd0dc569c56f9836bcf838a427c1307647a6
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-12-28 16:57:35 +0100

    4.2: Add custom validator.

--- src/OC/PlatformBundle/Entity/Advert.php
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -8,6 +8,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use OC\PlatformBundle\Validator\Antiflood;
 
 /**
  * Advert
@@ -57,6 +58,7 @@ class Advert
      *
      * @ORM\Column(name="content", type="text")
      * @Assert\NotBlank()
+     * @Antiflood()
      */
     private $content;
 
